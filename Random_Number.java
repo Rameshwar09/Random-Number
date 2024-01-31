572826f8966d92902495e35ae58ffb5d873c5b54
Rameshwar Shinde <107783092+Rameshwar09@users.noreply.github.com>
2024-01-31 15:15:38 +0530

    Add files via upload

---
+++ Random_Number.java
@@ -0,0 +1,32 @@
+package project;
+import java.util.*;
+public class Random_Number {
+	public static void main(String[] args) {
+        // Generate a random number between 1 and 10
+        int targetNumber = (int) (Math.random() * 10) + 1;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Guess a number between 1 and 10:");
+
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            // Read the user's guess
+            int userGuess = scanner.nextInt();
+
+            // Check if the guess is correct
+            if (userGuess == targetNumber) {
+                System.out.println("Congratulations! You guessed the correct number: " + targetNumber);
+                break;
+            } else if (userGuess < targetNumber) {
+                System.out.println("Try again! The number is larger than " + userGuess);
+            } else {
+                System.out.println("Try again! The number is smaller than " + userGuess);
+            }
+        }
+
+        // Close the scanner
+        scanner.close();
+	}
+}
